78756463bf6704a02bd87e60afe9850704201cb3
Aashish Panthi <aashishpanthi11@gmail.com>
2022-11-26 14:24:37 +0545

    remove auth screen

--- App.js
+++ App.js
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as SecureStore from "expo-secure-store";
 
 // Screens
-import Auth from "./screens/Auth";
 import Home from "./screens/Home";
 
 export default function App() {
-  const [defaultScreen, setDefaultScreen] = useState("Auth");
+  const [defaultScreen, setDefaultScreen] = useState("Home");
 
   const Stack = createNativeStackNavigator();
-
-  const getToken = async () => {
-    const token = await SecureStore.getItemAsync("token");
-
-    if (token) {
-      setDefaultScreen("Home");
-    }
-  };
-
-  useEffect(() => {
-    getToken();
-  }, []);
 
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName={defaultScreen}>
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{
-            headerShown: false,
-          }}
-        />
-
         <Stack.Screen
           name="Home"
           component={Home}
